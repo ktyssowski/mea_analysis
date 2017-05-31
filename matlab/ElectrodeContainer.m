bc27f3ad4948ef72dd0fd54e8d63cd73e8a5cdf5
Sam Rendall <sdrendall@gmail.com>
2017-05-31 19:32:32 -0400

    added help text to ElectrodeContainer.m

--- matlab/ElectrodeContainer.m
+++ matlab/ElectrodeContainer.m
@@ -18,8 +18,40 @@ classdef ElectrodeContainer < handle
         function obj = ElectrodeContainer(varargin)
             %% ElectrodeContainer([spike_index], varargin)
             %
-            %   Container for data associated with an electrode
-            % TODO add params here
+            % Container for data associated with an electrode
+            %   
+            % PARAMETERS
+            %
+            % spike_index - contains the [well_row, well_col, electrode_col, electrode_row] index 
+            %   corresponding to the electrode that the  ElectrodeContainer stores data for (TODO - rename this to electrode_index)
+            %
+            % spike_times - contains the timing of each spike (as a matlab datetime object) that occured on the electrode
+            %
+            % contains_data - true for electrodes containing a sufficient number of spikes for preprocessing. 
+            %   Electrodes with fewer than 20 spikes are ignored during preprocessing. 
+            %
+            % valid - n_clusters x 1 boolean array set using the 'flag' command on spike_analyzer. 
+            %   false for clusters the user has determined to be unusable
+            %
+            % class_no - num_clustering_results x 1 cell array containing the n_spikes x 1 classification arrays 
+            %   for each clustering. For example, to access the class of the 5th spike for a 3 cluster model, 
+            %   you can do this: spike_class = ElectrodeContainer.class_no{3}(5). 
+            %   The classification in this case would be a number from 1 - 3.
+            %
+            % features - struct containing spike features. features.pc_scores contains eigenvalues for 
+            %   the first 3 principal component.s this is the only feature that I use right now, 
+            %   although there are others computed during preprocessing. See get_spike_features.m
+            %
+            % cluster_model - num_clustering_results x 1 cell array containing model objects for each clustering. 
+            %   currently contains matlab gmdistribution objects
+            %
+            % n_clusters - the number of clusters determined to be present on an electrode. 
+            %   Use this as an index to the cluster_model and class_no fields to retrieve the 
+            %   appropriate cluster model object and spike classifications respectively.
+            %
+            % mean_waveforms - num_clustering_results x 1 cell array containing num_clusters x spike_dimensionality 
+            %   waveform matricies containing the mean waveforms for each cluster for each clustering result.
+
             parser = inputParser();
             parser.addOptional('spike_index', [], @isnumeric);
             parser.addOptional('spike_times', []);
