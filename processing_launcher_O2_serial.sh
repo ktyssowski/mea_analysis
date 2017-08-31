36e7dc8fde1f470a53f21a9eb13a69b383b77cce
Kate Letai <kcletai@gmail.com>
2017-08-31 16:28:29 -0400

    bash script for preprocessing on O2

---
+++ processing_launcher_O2_serial.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+
+module load matlab/2017a
+matlab -nodesktop -r "process_spk_files({'$(join_by \',\' $@)'}); exit"
+
