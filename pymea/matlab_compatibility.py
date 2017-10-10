4c502186e2c593420395fcdd87903af125166cac
Kate Letai <kcletai@gmail.com>
2017-10-10 11:35:08 -0400

    updated pymea

--- pymea/matlab_compatibility.py
+++ pymea/matlab_compatibility.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from datetime import timedelta
+import pandas as pd
 
 def datetime_str_to_datetime(datetime_str):
     """
@@ -39,3 +41,23 @@ def get_row_col_number_tuple(unit_name):
     return (row_num, col_num)
 
 
+def map_classic_to_lumos(cat_table, map_path, dest_path):
+    mapping = pd.read_csv(map_path)
+    for orig_str, new_str in zip(mapping['CytoView Well / Electrode'], mapping['48 Well Classic Well / Electrode']):
+        cat_table['unit_name'] = cat_table['unit_name'].str.replace(orig_str, new_str)
+    cat_table['unit_name'] = cat_table['unit_name'].str.replace("_", "")
+    cat_table.to_csv(dest_path)
+    return cat_table
+
+def remapped_str_to_datetime(datetime_str):
+    """
+    This converts the strings generated when a remapped cat_table is written to a table to python datetime objects
+    """
+    return datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+def create_stim_starts(stim_times):
+    '''Adds the date and seconds columns of the stim_times table, and returns a datetime series of the
+    stimulation tags'''
+    stim_times['date_time'] = stim_times['date_time'].map(datetime_str_to_datetime)
+    td=stim_times['seconds'].map(lambda x: timedelta(seconds = x))
+    return stim_times['date_time']+td
